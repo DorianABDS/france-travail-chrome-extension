2a28c9df20a9350422de14086ae45dccd3072262
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-07-06 20:32:28 +0200

    feat: enhance ESLint configuration and add lint script to package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,30 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        chrome: "readonly",
+      },
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+      },
+    },
+  },
+  {
+    files: ["sync-version.js"],
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+      },
+    },
+  },
 ]);
