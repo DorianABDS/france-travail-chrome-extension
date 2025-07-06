1118c8927ff0bf867fd69999b07a22f9f486b62e
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-07-06 20:46:07 +0200

    feat: move sync-version script to assets/script and update release script path

--- assets/script/sync-version.js
+++ assets/script/sync-version.js
@@ -1,17 +1,13 @@
-const fs = require('fs');
+import fs from 'fs';
 
-// Lire le fichier et extraire la version depuis package.json
 const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 const version = packageJson.version;
 
-// Définir le chemin vers manifest.json, lire et le parser
 const manifestPath = 'manifest.json';
 const manifestJson = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
 
-// Mettre à jour la propriété version dans manifest.json avec celle de package.json
 manifestJson.version = version;
 
-// Réécrire manifest.json avec la nouvelle version
 fs.writeFileSync(manifestPath, JSON.stringify(manifestJson, null, 2) + '\n');
 
 console.log(`Manifest version updated to ${version}`);
